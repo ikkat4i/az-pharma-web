4ee52c57de6a8db15ed3980cb076cb258f578142
ikkat4i <gautomie96@gmail.com>
2026-08-11 14:03:38 -0300

    Actualizar logo final del footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,7 +24,7 @@ export function Footer() {
 
         <section className="footer-brand-column">
           <Image
-            src="/images/az-pharma-footer-nuevo.png"
+            src="/images/az-pharma-footer-v3.png"
             alt="AZ+ PHARMA"
             width={160}
             height={100}
